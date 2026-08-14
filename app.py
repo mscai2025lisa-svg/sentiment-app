312b61d69c529ca371eac9071d96e2ec8c4a7ee3
mscai2025lisa-svg <mscai2025.lisa@unigoa.ac.in>
2026-08-14 13:48:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Sentiment Analysis API")
 # ---- Load model + tokenizer once, when the server starts ----
 print("Loading model... this happens once at startup")
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+MODEL_NAME = "lisaluis717/sentiment-distilbert-imdb"
 model.eval()
 print("Model loaded successfully!")
 
